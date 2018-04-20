4b8bd2de47f370280f77c6aa0380ce3a30898e46
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-04-20 16:33:46 +0200

    migration github api

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
-const express = require('express')
-const app = express()
+/**
+ * CONFIGURATION
+ *
+ * Pré-requis :
+ *  Configurer une variable d'environnement GITHUB_TOKEN
+ *
+ */
 
-app.get('/all', function (req, res) {
-    res.send('init all !')
-}).listen(8080, function () {
-    console.log('App starting on 8080')
-})
+var gh = new GitHub({
+    // variable d'environnement GITHUB_TOKEN à configurer dans Travis
+    token: process.env.GITHUB_TOKEN
+});
+
+const githubUser = 'DiginamicFormation';
+
+const config = {
+    "temp1" : 'gestion-du-transport',
+    "temp2" : 'gestion-des-absences',
+    "temp3" : 'gestion-des-missions',
+};
+
+
+/**
+ * Scripting
+ *
+ * Génération des issues.
+ *
+ */
+const fs = require('fs');
+const GitHub = require('github-api');
+
+Object.keys(config).forEach(repo => {
+    fs.readdirSync(`${config[repo]}/issues`).forEach(file => {
+        fs.readFile(`${config[repo]}/issues/${file}`, 'utf8', (err,message) => {
+            if (err) {
+                return console.log(err);
+            } else {
+                const title = file.replace('.md','');
+
+                gh.getIssues(githubUser, repo).createIssue({
+                    title : title,
+                    body: message
+                }).catch(console.log);
+
+            }
+        });
+    })
+});
--- scripts/config.js
+++
@@ -1,7 +0,0 @@
-exports.repos = [{
-    repoName : 'temp',
-    projectName : 'gestion-du-transport'
-}];
-
-exports.orgaGithub = 'DevInstitut';
-exports.protocolGithub = 'git@github.com:'; // git@github.com: ou https://github.com/
