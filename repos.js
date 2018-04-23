6fc054e8f71118bc14667a36c6aef41a07482c5d
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-04-23 12:24:52 +0200

    repos.js duplicate code

--- repos.js
+++ repos.js
@@ -1,4 +1,21 @@
-const actionRepo =(actionFn, gh, githubUser, config) => {
+exports.genRepos = (gh, githubUser, config) => {
+    const allCalls = [];
+
+    Object.keys(config).forEach(repoName => {
+
+        console.log(`** dépôts : ${repoName}-front et ${repoName}-back`);
+
+        const orga = gh.getOrganization(githubUser);
+
+        allCalls.push(orga.createRepo({ name:`${repoName}-front`}));
+        allCalls.push(orga.createRepo({ name:`${repoName}-back`}));
+    });
+
+    return Promise.all(allCalls);
+
+};
+
+exports.deleteRepos = (gh, githubUser, config) => {
 
     const allCalls = [];
 
@@ -8,23 +25,12 @@ const actionRepo =(actionFn, gh, githubUser, config) => {
 
         const orga = gh.getOrganization(githubUser);
 
-        allCalls.push(actionFn(orga)({ name:`${repoName}-front`}));
-        allCalls.push(actionFn(orga)({ name:`${repoName}-back`}));
+        allCalls.push(orga.deleteRepo({ name:`${repoName}-front`}));
+        allCalls.push(orga.deleteRepo({ name:`${repoName}-back`}));
     });
 
     return Promise.all(allCalls);
 
-
-
-};
-
-exports.genRepos = (gh, githubUser, config) => {
-    return actionRepo((orga) => orga.createRepo, gh, githubUser, config);
-};
-
-exports.deleteRepos = (gh, githubUser, config) => {
-
-    return actionRepo((orga) => orga.deleteRepo, gh, githubUser, config);
 };
 
 
