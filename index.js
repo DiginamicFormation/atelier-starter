60f0fb1926e36dff2eb53e5e384c3ec13d3ac23b
rbonnamy <rbonnamy@diginamic.fr>
2018-04-23 13:25:54 +0200

    gen issues gestion-des-missions

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const githubUser = 'javanger';
 // NOM DU DEPOT SUR GITHUB - PROJET ASSOCIE
 const config = {
  //   "gestion-des-transports" : 'gestion-des-transports',
-    "gestion-des-absences" : 'gestion-des-absences',
- //   "gestion-des-missions" : 'gestion-des-missions',
+ //   "gestion-des-absences" : 'gestion-des-absences',
+    "gestion-des-missions" : 'gestion-des-missions',
 };
 
 const GitHub = require('github-api');
