a2a01c136c50640544e249458795d2a0806aa01a
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-04-23 11:35:16 +0200

    gen repo

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const gh = new GitHub({
 });
 
 // pour générer les dépôts
-// require('./repos').genRepos(gh, githubUser, config).catch(console.log);
+require('./repos').genRepos(gh, githubUser, config).catch(console.log);
 
 // pour générer les issues
 // require('./repos').genRepos(gh, githubUser, config).catch(console.log);
