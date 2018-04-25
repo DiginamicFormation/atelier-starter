750aefecdd729f84d44b2c2fd6124f707be940ba
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-04-25 22:03:31 +0200

    abandon du sequentiel

--- issues.js
+++ issues.js
@@ -2,27 +2,16 @@ const fs = require('fs');
 
 exports.genIssues = (gh, githubUser, config) => {
 
-    const allPromiseIssues = [];
+    const allPromiseIssues$ = [];
 
     Object.keys(config).forEach(repo => {
-        const repoIssues = [];
         fs.readdirSync(`${config[repo]}/issues`).forEach(file => {
             const body = fs.readFileSync(`${config[repo]}/issues/${file}`, 'utf8');
             const title = file.replace('.md','');
-
-
-            repoIssues.push(() => {
-                console.log(`** Création issue sur le dépôt ${repo}-front : ${title} `);
-                return gh.getIssues(githubUser, `${repo}-front`).createIssue({title , body})
-            });
-
-
+            allPromiseIssues$.push(gh.getIssues(githubUser, `${repo}-front`).createIssue({title , body}));
         });
-
-        const promiseRepo$ =  repoIssues.reduce((isFn1, isFn2) => isFn1.then(() => isFn2()), Promise.resolve());
-        allPromiseIssues.push(promiseRepo$);
 
     });
 
-    return Promise.all(allPromiseIssues);
+    return Promise.all(allPromiseIssues$);
 };
