5806780d2ea8d8c5d1ca2078a93c4c7751af9688
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-04-26 17:47:59 +0200

    issues en version synchrone

--- issues.js
+++ issues.js
@@ -1,18 +1,30 @@
 const fs = require('fs');
 
+async function call(fns) {
+
+    for(let f of fns) {
+        await f();
+    }
+}
+
 exports.genIssues = (gh, githubUser, config) => {
 
-    const allPromiseIssues$ = [];
+    const allPromiseIssuesFn$ = [];
 
     Object.keys(config).forEach(repo => {
         fs.readdirSync(`${config[repo]}/issues`).forEach(file => {
             const body = fs.readFileSync(`${config[repo]}/issues/${file}`, 'utf8');
             const title = file.replace('.md','');
-            console.log(`** Création issue sur le dépôt ${repo}-front : ${title} `);
-            allPromiseIssues$.push(gh.getIssues(githubUser, `${repo}-front`).createIssue({title , body}));
+
+            allPromiseIssuesFn$.push(() => {
+                console.log(`** Création issue sur le dépôt ${repo}-front : ${title} `);
+                return gh.getIssues(githubUser, `${repo}-front`).createIssue({title , body})
+            });
         });
 
     });
 
-    return Promise.all(allPromiseIssues$);
+
+
+    return call(allPromiseIssuesFn$);
 };
