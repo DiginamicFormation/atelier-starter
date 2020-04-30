9faa1f934ad1da9e6e7f9005be0ea5c7314af4aa
Rossi Oddet <rossi.oddet@dev-institut.fr>
2020-04-30 12:45:30 +0200

    2020-D02-java

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     // Exemple : "gdm", "gestion-des-missions"
     // Dans le cas d'une génération de dépôt, 2 dépôts sont générés :
     // Exemple : "gdm-front" et "gdm-back "
-    repositoryName: "groupe-3",
+    repositoryName: "groupe-1",
 
     /*
     Le nom du projet.
