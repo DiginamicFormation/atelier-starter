2d5f3bbc573a8503fc5710a63adf1ae01a144ec6
Rossi Oddet <rossi.oddet@dev-institut.fr>
2020-08-26 05:26:20 +0200

    Génération Transport

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     // Exemple : "gdm", "gestion-des-missions"
     // Dans le cas d'une génération de dépôt, 2 dépôts sont générés :
     // Exemple : "gdm-front" et "gdm-back "
-    repositoryName: "test-4",
+    repositoryName: "gestion-des-transports",
 
     /*
     Le nom du projet.
